3b5e5cd9ca02b31eacef3badb484a2c4be92c97b
Benjamin <b.girardpro@gmail.com>
2021-12-16 17:33:10 +0100

    Add delete et get by id route

--- route/subscriberRouter.js
+++ route/subscriberRouter.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const subscriberController = require('../controller/subscriberController');
 
 router.get('/', subscriberController.getMany);
+router.get('/:id', subscriberController.getOneById);
 router.post('/', subscriberController.postOne);
 router.put('/:id', subscriberController.updateOne);
+router.delete('/:id', subscriberController.deleteOne);
 
 module.exports = router;
