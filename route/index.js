3a920b72751d1003aa0ddf6729b0359b335e08a5
Benjamin <b.girardpro@gmail.com>
2021-12-20 11:59:21 +0100

    Add auth route

--- route/index.js
+++ route/index.js
@@ -1,7 +1,9 @@
 const subscriberRouter = require('./subscriberRouter');
+const authRouter = require('./authRouter');
 
 const router = (app) => {
   app.use('/subscribers', subscriberRouter);
+  app.use('/auth', authRouter);
 };
 
 module.exports = { router };
