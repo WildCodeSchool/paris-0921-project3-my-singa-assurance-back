00cd222363499bb32716deb926dea248c63ef340
Benjamin <b.girardpro@gmail.com>
2021-12-20 17:28:59 +0100

    Add recipient route to root router

--- route/index.js
+++ route/index.js
@@ -1,8 +1,10 @@
 const subscriberRouter = require('./subscriberRouter');
+const recipientRouter = require('./recipientRouter');
 const authRouter = require('./authRouter');
 
 const router = (app) => {
   app.use('/subscribers', subscriberRouter);
+  app.use('/recipients', recipientRouter);
   app.use('/auth', authRouter);
 };
 
