9f9c36238dbd7ee9c877a19cebb9d862d90ae6ee
Benjamin <b.girardpro@gmail.com>
2022-01-08 18:35:58 +0100

    Add error when it is threw

--- middleware/handleInternalError.js
+++ middleware/handleInternalError.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
-  res.status(500).send('An internal server error occured');
+  res.status(500).send(`Internal Server Error: ${err.message}`);
 };
