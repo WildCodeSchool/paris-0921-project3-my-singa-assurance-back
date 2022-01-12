054d9ded8ca893627c7bf713e46cf9c268d2188f
Benjamin <b.girardpro@gmail.com>
2022-01-12 21:14:51 +0100

    Refactoring of middleware placement

--- route/index.js
+++ route/index.js
@@ -2,9 +2,11 @@ const subscriberRouter = require('./subscriberRouter');
 const recipientRouter = require('./recipientRouter');
 const authRouter = require('./authRouter');
 
+const { checkCredentials } = require('../controller/authController');
+
 const router = (app) => {
-  app.use('/subscribers', subscriberRouter);
-  app.use('/recipients', recipientRouter);
+  app.use('/subscribers', checkCredentials, subscriberRouter);
+  app.use('/recipients', checkCredentials, recipientRouter);
   app.use('/auth', authRouter);
 };
 
