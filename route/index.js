c3cbecd22e05885453ec706205535fd97f29eb73
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-12 16:02:56 +0100

    added optionroute in index.js

--- route/index.js
+++ route/index.js
@@ -1,11 +1,13 @@
 const subscriberRouter = require('./subscriberRouter');
 const recipientRouter = require('./recipientRouter');
 const authRouter = require('./authRouter');
+const optionRouter = require('./optionRouter');
 
 const router = (app) => {
   app.use('/subscribers', subscriberRouter);
   app.use('/recipients', recipientRouter);
   app.use('/auth', authRouter);
+  app.use('/options', optionRouter);
 };
 
 module.exports = { router };
