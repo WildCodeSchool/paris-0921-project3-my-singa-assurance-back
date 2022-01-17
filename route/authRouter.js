ae3f979ccfe8fedea84b27cbe71027f3d21e7413
Benjamin <b.girardpro@gmail.com>
2022-01-17 10:24:45 +0100

    Add verification if email exist or not

--- route/authRouter.js
+++ route/authRouter.js
@@ -3,6 +3,7 @@ const asyncHandler = require('express-async-handler');
 
 const authController = require('../controller/authController');
 
+router.get('/verify/email', asyncHandler(authController.checkIfEmailExists));
 router.post('/login', asyncHandler(authController.logIn));
 router.post('/register', asyncHandler(authController.register));
 
